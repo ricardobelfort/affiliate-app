3b8f65b51e4c8f2ceef46c06589b80e07299078b
Ricardo Belfort <rbelfort2004@gmail.com>
2025-12-01 22:00:06 -0400

    fix: ajustes de environment

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  supabase: {
+    url: 'https://zpxzheflrrnnzvbxbjbi.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpweHpoZWZscnJubnp2YnhiamJpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ2MDkyMzIsImV4cCI6MjA4MDE4NTIzMn0.StjzIub2qXszbChQdqG-_J62wSOWFwmLcIzzwM0ZHkQ'
+  }
+};
